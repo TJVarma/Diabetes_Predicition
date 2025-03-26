41f8b2ebfde214244787517f711de05cd5d1582f
TJ Varma <tjvarma@example.com>
2025-03-26 18:12:08 -0400

    changes

--- 1_gui.py
+++ 1_gui.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score
